c08ac26fca906d444a7cf033b2072b5a33796e59
front-ed <edwinschutjes@gmail.com>
2023-11-12 14:03:24 +0100

    add: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 function App() {
   return (
     <main>
-      <p>hello canicas</p>
+      <Header />
     </main>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/DEF_CanicasFlavours_logo_koper_RGB.svg";
+
+const Header = () => {
+  return (
+    <figure className="logo-wrapper">
+      <img src={logo} alt="logo" />
+    </figure>
+  );
+};
+
+export default Header;
