cefef4be0c81c017e922f4689700906009ed8bcd
EdwinSch <edwinschutjes@gmail.com>
2023-11-13 10:13:58 +0100

    edit: add to header component

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,12 @@ import Item from "./Item";
 const Grid = () => {
   return (
     <>
-      <p className="viewport-error">Browser too small</p>
+      <p className="viewport-error">
+        Please view this site on a bigger window or device.
+      </p>
+
+      <h1>december assortiment 2023</h1>
+      {/* Grid map */}
       <section className="assortment-container">
         {assortment.map((item) => {
           return <Item key={item.id} {...item} />;
